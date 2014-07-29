63de92174d64c9158a25ab4237c3ecd417a3c403
Simone D'Amico <dymissy86@gmail.com>
2014-07-29 12:21:19 +0000

    Ita Translation

---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,108 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>design/standard/extract</name>
+    <message>
+        <source>Extract</source>
+        <translation>Esporta</translation>
+    </message>
+    <message>
+        <source>Extract settings</source>
+        <translation>Esporta impostazioni</translation>
+    </message>
+    <message>
+        <source>Row separator</source>
+        <translation>Separatore di riga</translation>
+    </message>
+    <message>
+        <source>Column separator</source>
+        <translation>Separatore di colonna</translation>
+    </message>
+    <message>
+	<source>Options</source>
+	<translation>Opzioni</translation>
+    </message>
+    <message>
+	<source>Limit</source>
+	<translation>Numero di elementi</translation>
+    </message>
+    <message>
+	<source>Offset</source>
+	<translation>Offset</translation>
+    </message>
+    <message>
+        <source>Selected node</source>
+        <translation>Nodi selezionati</translation>
+    </message>
+    <message>
+        <source>Selected attribute(s)</source>
+        <translation>Attributi selezionati</translation>
+    </message>
+    <message>
+        <source>Selected class</source>
+        <translation>Classe selezionata</translation>
+    </message>
+    <message>
+        <source>Choose node to export</source>
+        <translation>Seleziona nodo da esportare</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Cambia</translation>
+    </message>
+    <message>
+        <source>Position</source>
+        <translation>Posizione</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Identifier</source>
+        <translation>Identificativo</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Aggiorna</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Download</translation>
+    </message>
+    <message>
+        <source>Add attribute</source>
+        <translation>Aggiungi attributo</translation>
+    </message>
+    <message>
+        <source>Remove selected attribute(s)</source>
+        <translation>Rimuovi attributi selezionati</translation>
+    </message>
+    <message>
+        <source>Select a node to export</source>
+        <translation>Seleziona un nodo da esportare</translation>
+    </message>
+    <message>
+        <source>Setup format options</source>
+        <translation>Seleziona le opzioni da formattare</translation>
+    </message>
+    <message>
+        <source>Select concerned class</source>
+        <translation>Seleziona le classi</translation>
+    </message>
+    <message>
+	<source>Add/Remove attributes</source>
+	<translation>Aggiungi/Rimuovi attributi</translation>
+    </message>
+    <message>
+        <source>Click the download button</source>
+        <translation>Click sul pulsante download</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/navigationpart</name>
+    <message>
+        <source>Extract</source>
+        <translation>Esporta</translation>
+    </message>
+</context>
+</TS>
